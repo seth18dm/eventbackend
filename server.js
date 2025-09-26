779830cc96050d632e86d71e5c487210d7308933
seth18dm <digambarmarathe.9@gmail.com>
2025-09-26 12:32:56 +0530

    added cors link

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ if (!fs.existsSync(uploadDir)) {
 
 // Middleware
 app.use(cors({
-  origin: ['https://eventfrontend-main.onrender.com', 'http://localhost:3000'],
+  origin: ['https://eventfrontend-main.onrender.com', 'http://localhost:3000', 'https://artiststation.co.in'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
